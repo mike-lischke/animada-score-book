a566ec1f3d5dc9c57b3ddf173e64ab8253f039c7
Mike Lischke <mike@lischke-online.de>
2026-05-05 21:23:58 +0200

    Added missing copyright note
    Signed-off-by: Mike Lischke <mike@lischke-online.de>

--- tests/e2e/playback-display.spec.ts
+++ tests/e2e/playback-display.spec.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Mike Lischke. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
 import { expect, test } from "@playwright/test";
 
 import { expectPlaybackToMove, routeApi } from "./helpers.js";
